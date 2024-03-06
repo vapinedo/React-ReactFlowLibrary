8d1a3437f2715f787a811408d8144a6033079ee8
Victor Pinedo <victorpinedomartinez@gmail.com>
2024-03-06 12:48:33 -0500

    adding some built-in hooks for interactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import ReactFlow from "reactflow"
 import "reactflow/dist/style.css"
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow"
+import { useState } from "react";
+import { useCallback } from "react";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1"} },
@@ -9,9 +11,24 @@ const initialNodes = [
 const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 export default function App() {
+
+  const [nodes, setNodes, onNodesChange] = useState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useState(initialEdges);
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+  
   return (
-    <article  >
-      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    <article style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow 
+        nodes={initialNodes} 
+        edges={initialEdges} 
+        onConnect={onConnect}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+      />
     </article>
   )
 }
